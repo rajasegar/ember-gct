9781c3d0dda2ed651b28a3e9acf3a829ba06d8b7
Rajasegar Chandran <rajasegar.c@gmail.com>
2023-11-08 18:44:30 +0530

    chore: refactor generate component test

--- src/components/generateTest.js
+++ src/components/generateTest.js
@@ -1,8 +1,41 @@
-import { preprocess, Walker, print } from '@glimmer/syntax';
+import { preprocess, Walker, print, traverse } from '@glimmer/syntax';
 import { readFileSync, existsSync } from 'node:fs';
 import * as R from 'ramda';
 
+function getComponentProps(root) {
+  const componentProps = [];
+  traverse(root, {
+    MustacheStatement: function (node) {
+      if (node.path.original.startsWith('@')) {
+        // console.log(node.path.original);
+        componentProps.push(node.path.original);
+      }
+    }
+  });
+
+  return componentProps;
+}
+
+function getDomSelectors(root) {
+  const selectors = [];
+  traverse(root, {
+    ElementNode: function (node) {
+      const classAttr = node.attributes.find((a) => a.name === 'class');
+      let selector = node.tag;
+
+      // Add class attr value only if it is a TextNode
+      if (classAttr && classAttr.value.type === 'TextNode') {
+        selector = `${node.tag}.${classAttr.value.chars.replace(' ', '.')}`;
+        selectors.push(selector);
+      }
+    }
+  });
+
+  return selectors;
+}
+
 function walkAst(ast) {
+  const domElements = [];
   let walker = new Walker();
   walker.visit(ast, function (node) {
     if (
@@ -10,6 +43,7 @@ function walkAst(ast) {
       (node.path.original === 'if' || node.path.original === 'else')
     ) {
       walkAst(node.program);
+      processNode(node.program);
 
       if (node.inverse) {
         walkAst(node.inverse);
@@ -24,7 +58,7 @@ function walkAst(ast) {
       // Add class attr value only if it is a TextNode
       if (classAttr && classAttr.value.type === 'TextNode') {
         selector = `${node.tag}.${classAttr.value.chars.replace(' ', '.')}`;
-        // domElements.push(selector);
+        domElements.push(selector);
       }
 
       // generate asserts for data-test elements
@@ -39,66 +73,100 @@ function walkAst(ast) {
     }
   });
 
-  return;
+  return domElements;
 }
 
-export default function (componentName, astNodes) {
-  const root = '/Users/rajasegarchandran/Code/unity_frontend';
+function generateAssertsForDom(root, componentName) {
   const components = `${root}/app/components`;
+  const hbsFile = `${components}/${componentName}/template.hbs`;
 
-  function generateAssertsForDom(componentName) {
-    const hbsFile = `${components}/${componentName}/template.hbs`;
+  let asserts = [];
 
-    const domElements = [];
-    let asserts = [];
+  // console.log('Template: ', hbsFile);
+  const hbsFound = existsSync(hbsFile);
+  if (hbsFound) {
+    console.log('Template: ', hbsFile);
+    const data = readFileSync(hbsFile, 'utf-8');
+    let ast = preprocess(data);
 
-    const hbsFound = existsSync(hbsFile);
-    if (hbsFound) {
-      const data = readFileSync(hbsFile, 'utf-8');
-      let ast = preprocess(data);
+    const domElements = walkAst(ast);
 
-      walkAst(ast);
-
-      asserts = R.uniq(domElements).map(
-        (el) => `assert.dom('${el}').exists();`
-      );
-    }
-    return asserts;
+    asserts = R.uniq(domElements).map((el) => `assert.dom('${el}').exists();`);
   }
 
-  let needSinon = false;
+  return asserts;
+}
 
-  const generateTests = () => {
-    return astNodes.map((node, i) => {
-      const fakes = [];
-      node.attributes.forEach((attr) => {
-        if (attr.value.type === 'MustacheStatement') {
-          const { path, params } = attr.value;
-          if (path.type === 'PathExpression') {
-            if (path.original === 'action') {
-              needSinon = true;
-              let actionName = '';
-              const [firstParam] = params;
-              if (firstParam.type === 'StringLiteral') {
-                actionName = `${firstParam.value}Action`;
-              }
-              if (firstParam.type === 'PathExpression') {
-                actionName = `${firstParam.parts[0]}Action`;
-              }
+function captureInfo(root, componentName) {
+  const components = `${root}/app/components`;
+  const hbsFile = `${components}/${componentName}/template.hbs`;
 
-              if (actionName) {
-                fakes.push(`this.${actionName} =  sinon.fake();`);
-                attr.value.path.original = `this.${actionName}`;
-                attr.value.this = true;
-                attr.value.parts = [actionName];
-                attr.value.params = [];
-              }
+  let asserts = [];
+
+  const hbsFound = existsSync(hbsFile);
+  if (hbsFound) {
+    const data = readFileSync(hbsFile, 'utf-8');
+    let ast = preprocess(data);
+
+    const variations = [];
+
+    traverse(ast, {
+      BlockStatement: {
+        enter(node) {
+          if (node.path.original === 'if') {
+            const componentProps = getComponentProps(node.program);
+            const selectors = getDomSelectors(node.program);
+            variations.push({ componentProps, selectors });
+          }
+          if (node.inverse) {
+            const componentProps = getComponentProps(node.program);
+            const selectors = getDomSelectors(node.program);
+            variations.push({ componentProps, selectors });
+          }
+        },
+
+        exit(node) {}
+      }
+    });
+
+    console.log(variations);
+  }
+
+  return asserts;
+}
+
+function generateTests() {
+  return astNodes.map((node, i) => {
+    const fakes = [];
+    node.attributes.forEach((attr) => {
+      if (attr.value.type === 'MustacheStatement') {
+        const { path, params } = attr.value;
+        if (path.type === 'PathExpression') {
+          if (path.original === 'action') {
+            needSinon = true;
+            let actionName = '';
+            const [firstParam] = params;
+            if (firstParam.type === 'StringLiteral') {
+              actionName = `${firstParam.value}Action`;
+            }
+            if (firstParam.type === 'PathExpression') {
+              actionName = `${firstParam.parts[0]}Action`;
+            }
+
+            if (actionName) {
+              fakes.push(`this.${actionName} =  sinon.fake();`);
+              attr.value.path.original = `this.${actionName}`;
+              attr.value.this = true;
+              attr.value.parts = [actionName];
+              attr.value.params = [];
             }
           }
         }
-      });
-      const asserts = generateAssertsForDom(componentName);
-      const test = `
+      }
+    });
+
+    const asserts = generateAssertsForDom(root, componentName);
+    const test = `
 test('Should render the ${componentName} component - variant ${i}', async function (assert) {
     ${fakes.join('\n')}
     await render(hbs\`${print(node)}\`);
@@ -107,12 +175,17 @@ test('Should render the ${componentName} component - variant ${i}', async functi
   });
 `;
 
-      return test;
-    });
-  };
+    return test;
+  });
+}
 
-  const tests = generateTests().join('\n');
+export default function (root, componentName, astNodes) {
+  let needSinon = false;
 
+  // const tests = generateTests().join('\n');
+  const tests = '';
+
+  captureInfo(root, componentName);
   const str = `import { hbs } from 'ember-cli-htmlbars';
 import { module, test } from 'qunit';
 import { setupRenderingTest } from "ember-qunit";
--- src/components/runSingleTest.js
+++ src/components/runSingleTest.js
@@ -7,13 +7,14 @@ export default function runSingleHelperTest(withoutTests, root) {
   // const components = `${root}/app/components`;
   const integrationTests = `${root}/tests/integration/components`;
   const chance = new Chance();
-  const random = chance.pickone(withoutTests);
+  // const random = chance.pickone(withoutTests);
+  const random = 'accessible-icon-element';
   console.log('Random component: ', random);
   const astNodes = scanProject({ name: random }, root);
-  const testContent = generateComponentTests(random, astNodes);
+  const testContent = generateComponentTests(root, random, astNodes);
 
   const newTestFile = `${integrationTests}/${random}-test.js`;
   console.log(newTestFile);
 
-  formatAndWrite(newTestFile, testContent);
+  // formatAndWrite(newTestFile, testContent);
 }
